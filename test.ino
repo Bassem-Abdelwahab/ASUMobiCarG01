07b1ac4ac4a2ffce20f443122929d3a43dd2ad27
Ahmed-Abdalla-2020 <36240815+Ahmed-Abdalla-2020@users.noreply.github.com>
2018-02-19 21:00:03 +0200

    test
    test

---
+++ test.ino
@@ -0,0 +1,28 @@
+int red=10;
+int yellow=11;
+int green=12;
+void setup() {
+  pinMode(red,OUTPUT);
+  pinMode(yellow,OUTPUT);
+  pinMode(green,OUTPUT);
+}
+
+void loop() {
+
+  digitalWrite(red,HIGH);
+  digitalWrite(yellow,LOW);
+  digitalWrite(green,LOW);
+  delay(2000);
+
+  
+  digitalWrite(yellow,HIGH);
+  digitalWrite(red,LOW);
+  digitalWrite(green,LOW);
+  delay(2000);
+  
+  digitalWrite(green,HIGH);
+  digitalWrite(red,LOW);
+  digitalWrite(yellow,LOW);
+  delay(2000);
+  
+}
